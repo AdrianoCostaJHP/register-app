76d09809f6a690a85ca428a556dbe1589113923e
Adriano <adrianocostajhp@gmail.com>
2022-03-27 19:04:25 -0300

    ✨ implement the sidebar component

---
+++ app/atomic/organisms/SideBar/index.tsx
@@ -0,0 +1,72 @@
+import { colors } from "@app/common/theme/colors"
+import { useAuth } from "@app/features/auth/hooks/useAuth"
+import { Flex, Box, Img, Text, Button, Divider } from "@chakra-ui/react"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { routes } from "./routes"
+
+const SideBar = () => {
+
+  const router = useRouter()
+  const {logout} = useAuth()
+
+  return (
+    <Flex 
+      h="100vh" 
+      bg="white" 
+      minW="136px"
+      padding="1rem 0px"
+      direction="column" 
+      boxShadow="0px 4px 10px 0px rgba(0, 0, 0, 0.1)"
+      justifyContent="space-between"
+    >
+      <Box>
+        <Flex justifyContent="center">
+          <Img w="3.5rem" height="3.5rem" src="/assets/logo.svg" alt="logo"/>
+        </Flex>
+        <Flex direction="column" h="100%">
+          {routes?.map((route, index) => (
+            <Flex 
+              key={index} 
+              color={router.asPath === route.path ? 'primary' : 'grey.100'}
+              direction="column"
+            >
+              <Flex w="100%"  padding="1.5rem 0px" justifyContent="space-between" css={{
+                ':before':{
+                  content: '""',
+                  ...(router.asPath === route?.path && {
+                    width: '4px',
+                    height: '100%',
+                    marginLeft:'1px',
+                    background:colors.primary,
+                    borderRadius:'30px'
+                  })
+                },
+                ':after':{
+                  content: '""',
+                }
+              }}>
+                <Link href={route?.path}>
+                  <Flex direction="column" alignItems="center" gap="0.5rem" padding="0.5rem 0px">
+                    <Img w="24px" h="24px" src={`/assets/${route?.icon}`} alt={`sideBar-${index}`}/>
+                    <Text flexWrap="wrap" fontSize="md" fontWeight="light">{route?.name}</Text>
+                  </Flex>
+                </Link>
+              </Flex>
+              <Divider w="80%" margin="0 auto"/>
+            </Flex>
+          ))}
+        </Flex>
+      </Box>
+
+      <Flex justifyContent="center">
+        <Button variant="unstyled" onClick={logout}>
+          <Img w="2rem" height="2rem" src="/assets/logout.svg" alt="logout"/>
+        </Button>
+      </Flex>
+
+    </Flex>
+  )
+}
+
+export default SideBar
---
+++ app/atomic/organisms/SideBar/routes.ts
@@ -0,0 +1,12 @@
+export const routes = [
+  {
+    path: '/admin/dashboard',
+    name:'Dashboard',
+    icon:'dashboard.svg'
+  },
+  {
+    path: '/admin/registers',
+    name: 'Meus Registros',
+    icon:'note.svg'
+  }
+]
