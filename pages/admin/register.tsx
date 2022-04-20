8d0c24e4077c5138e759e838383c3268aeb03eea
Adriano <adrianocostajhp@gmail.com>
2022-04-20 19:29:47 -0300

    ✨ Implement registry components

--- pages/admin/register.tsx
+++ pages/admin/register.tsx
@@ -1,44 +1,85 @@
-import { RegisterDrawer } from "@app/atomic/organisms/RegisterDrawer"
-import RegisterList from "@app/atomic/organisms/RegisterList"
-import AdminTemplate from "@app/atomic/templates/AdminTemplate"
-import { Button, Flex, Text, useDisclosure } from "@chakra-ui/react"
+import RegisterList from "@app/atomic/organisms/RegisterList";
+import AdminTemplate from "@app/atomic/templates/AdminTemplate";
+import { useAuthContext } from "@app/features/auth/context/authContext";
+import { RegisterForm } from "@app/atomic/organisms/RegisterForm";
+import { useCreateRegisteredTimeMutation } from "@app/features/register/mutations.generated";
+import { Button, Flex, Text, useDisclosure } from "@chakra-ui/react";
+import { format, formatISO } from "date-fns";
+import { useCallback } from "react";
+import { toast } from "react-toastify";
 
 const Register = () => {
-  const {isOpen, onToggle, onClose} = useDisclosure()
+  const { isOpen, onToggle, onClose } = useDisclosure();
+  const { user } = useAuthContext()
+
+  const [createRegisteredTime] = useCreateRegisteredTimeMutation()
+
+  const onSubmit = useCallback(async (values) => {
+    try {
+      const formatDate = format(new Date(values?.register), "yyyy-MM-dd'T'HH:mm:ss'Z'")
+      await createRegisteredTime({
+        variables:{
+          data:{
+            timeRegistered: formatDate,
+            user: "2"
+          }
+        }
+      })
+      toast.success('Registro criado com sucesso!', {
+        position: "top-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } catch (error) {
+      toast.error('Ocorreu um erro ao fazer o registro!', {
+        position: "top-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  }, [createRegisteredTime]);
 
   return (
-   <AdminTemplate>
-     <RegisterDrawer isOpen={isOpen} onClose={onClose} />
-      <Flex 
-        w="100%" 
-        padding="2.5rem 9rem 6rem" 
-        direction="column"  
+    <AdminTemplate>
+      <RegisterForm isOpen={isOpen} onClose={onClose} onSubmit={onSubmit} />
+      <Flex
+        w="100%"
+        padding="2.5rem 9rem 6rem"
+        direction="column"
         justifyContent="center"
         alignItems="center"
       >
         <Flex w="100%" marginBottom="2rem" justifyContent="flex-end">
-          <Button 
+          <Button
             height="4rem"
-            padding="1rem 1.5rem" 
-            borderRadius="none" 
+            padding="1rem 1.5rem"
+            borderRadius="none"
             onClick={onToggle}
             _hover={{}}
             bg="primary"
           >
-            <Text 
+            <Text
               fontFamily="Montserrat"
               fontSize="xl"
-              fontWeight="black" 
+              fontWeight="black"
               color="white"
             >
               Registrar
             </Text>
           </Button>
         </Flex>
-        <RegisterList/>
+        <RegisterList />
       </Flex>
     </AdminTemplate>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
