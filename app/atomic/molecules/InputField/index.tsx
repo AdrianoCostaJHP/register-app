e5718e79092a84e1014a784e83de41ed72c43c11
Adriano <adrianocostajhp@gmail.com>
2022-04-20 19:27:37 -0300

    🎨 Format code with eslint rules

--- app/atomic/molecules/InputField/index.tsx
+++ app/atomic/molecules/InputField/index.tsx
@@ -1,61 +1,54 @@
-import React, {useRef, useEffect, useState, useCallback} from "react"
+import React, { useRef, useEffect, useState, useCallback } from "react";
 import { useField } from "@unform/core";
 import { FormControl, FormLabel, Input, InputProps } from "@chakra-ui/react";
 import { colors } from "../../../common/theme/colors";
 
-
 type InputFieldProps = {
   name: string;
-  label: string
-} & InputProps
+  label?: string;
+} & InputProps;
 
-const InputField = ({ name,label, ...rest}: InputFieldProps) => {
-
+const InputField = ({ name, label, ...rest }: InputFieldProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const { fieldName,  registerField, defaultValue, error} = useField(name)
-  const [isFocused, setIsFocused] = useState(false)
-  const [isFilled, setIsFilled] = useState(false)
+  const { fieldName, registerField, defaultValue, error } = useField(name);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
 
-  useEffect(()=> {
+  useEffect(() => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
-    })
-  },[ fieldName, registerField])
+      path: "value",
+    });
+  }, [fieldName, registerField]);
 
-  const handleInputBlur = useCallback( () => {
-    setIsFocused(false)
-    setIsFilled(!!inputRef.current?.value)
-    
-  },[])
-  
-  const handleInputFocus = useCallback( () => {
-    setIsFocused(true)
-    
-  },[])
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
 
-  return(
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  return (
     <FormControl id={name}>
-      <FormLabel fontSize={18} >
-        {label}
-      </FormLabel>
-    
-    <Input 
-      background={colors.grey[50]}
-      color={colors.grey[200]}
-      borderRadius="none"
-      border="none"
-      w="100%"
-      defaultValue={defaultValue} 
-      onFocus={handleInputFocus} 
-      onBlur={handleInputBlur} 
-      ref={inputRef} 
-      {...rest} 
-    />
-    </FormControl>
-    
-  )
-}
+      {label && <FormLabel fontSize={18}>{label}</FormLabel>}
 
-export default InputField
+      <Input
+        background={colors.grey[50]}
+        color={colors.grey[200]}
+        borderRadius="none"
+        border="none"
+        w="100%"
+        defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        ref={inputRef}
+        {...rest}
+      />
+    </FormControl>
+  );
+};
+
+export default InputField;
