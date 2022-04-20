9c14aca9e9ff9de17b47df7f7ecd486719b7126a
Adriano <adrianocostajhp@gmail.com>
2022-04-20 19:25:06 -0300

    ✨ Add header with authentication parameters

--- app/apollo/index.tsx
+++ app/apollo/index.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
+import React from "react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  createHttpLink
+  createHttpLink,
 } from "@apollo/client";
+import { parseCookies } from "nookies";
+
+const tokens = parseCookies();
+const token = tokens['@AuthRegisterToken']
 
 const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_API_URL,
   fetch: fetch,
-})
-
+  headers: token ? {
+    authorization: `Bearer ${token}`,
+  }: {},
+});
 
 const client = new ApolloClient({
   link: httpLink,
   ssrMode: !process.browser,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 export const CustomApolloProvider = ({ children }: any) => {
-  return (
-    <ApolloProvider client={client}>{children}</ApolloProvider>
-  )
-}
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
