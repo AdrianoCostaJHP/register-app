ea1de6e814939b052ac9263cfb5731713ebb8186
Adriano <adrianocostajhp@gmail.com>
2022-05-03 14:20:55 -0300

    ✨ Add authentication functions to routes

--- app/atomic/organisms/SideBar/routes.ts
+++ app/atomic/organisms/SideBar/routes.ts
@@ -1,12 +1,18 @@
+import { GetRolesQuery } from './../../../features/auth/queries.generated';
+import { GetRolesDocument } from "@app/features/auth/queries.generated";
+import { RolesEnum } from '@app/common/utils/roles';
+
 export const routes = [
   {
     path: '/admin/dashboard',
     name:'Dashboard',
-    icon:'dashboard'
+    icon:'dashboard',
+    roles: [RolesEnum.Admin]
   },
   {
     path: '/admin/register',
     name: 'Meus Registros',
-    icon:'note'
+    icon:'note',
+    roles: [RolesEnum.Admin, RolesEnum.User]
   }
 ]
