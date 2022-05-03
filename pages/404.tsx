be1e94c35c803a19a49249b1122d1282e9700d29
Adriano <adrianocostajhp@gmail.com>
2022-05-03 14:29:14 -0300

    💄 Refactor color scheme

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,10 @@
-import { colors } from "@app/common/theme/colors"
 import { Box, Flex, Img, Text } from "@chakra-ui/react"
-import { url } from "inspector"
 
 const Erro404 = () => {
   return (
-    <Flex direction="column" bg={colors.secondary} alignItems="center" justifyContent="center" height="100vh">
+    <Flex direction="column" bg="secondary" alignItems="center" justifyContent="center" height="100vh">
       <Img height="20%"src="/assets/logo.svg" alt="Logo"/>
-      <Box textAlign="center" color={colors.primary} >
+      <Box textAlign="center" color="primary" >
         <Text fontFamily="heading" fontSize={{base:'4xl', lg:'9xl'}}>Erro 404</Text>
         <Text fontSize="2xl">Não foi possível encontrar a página</Text>
       </Box>
